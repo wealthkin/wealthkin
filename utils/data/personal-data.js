b785f60360fe85ac2aebc6f66d7ffc7f742b21dc
wealthkin <167686818+wealthkin@users.noreply.github.com>
2024-05-19 22:33:20 +0800

    Update personal-data.js

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -2,7 +2,7 @@ export const personalData = {
   name: "WELL KIN MOLINA ANGARA",
   profile: '/profile.jpg',
   designation: "Software Developer",
-  description: "My name is WELL KIN ANGARA. I am a professional and enthusiastic programmer. I am a quick learner with a passion for self-learning and problem-solving. I love exploring new technologies, especially in web application development, and I primarily work with JavaScript. However, now I want to join the Bureau of Fire Protection (BFP). I am open to job opportunities that match my skills and interests.",
+  description: "My name is WELL KIN ANGARA. I am a programmer. I am a learner with a passion for self-learning and problem-solving. I love exploring new technologies, especially in web application development, and I primarily work with JavaScript. However, now I want to join the Bureau of Fire Protection (BFP). I am open to job opportunities that match my skills and interests.",
   email: 'lordewealthkin@gmail.com',
   phone: '+63 9625959952',
   address: 'Casiguran Aurora',
